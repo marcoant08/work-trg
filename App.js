50b2f9715914f465acad1bf9b7942b9e3140facf
marcoant08 <marcoant008@gmail.com>
2020-11-16 22:52:59 -0300

    with login bug

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import AuthProvider from "./src/contexts/auth";
+import { NavigationContainer } from "@react-navigation/native";
+import Routes from "./src/routes";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <NavigationContainer>
+        <AuthProvider>
+          <Routes />
+          <StatusBar style="auto" />
+        </AuthProvider>
+      </NavigationContainer>
     </View>
   );
 }
@@ -14,8 +21,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
